439e421b3832c85ef451778c974dbb22e9317d60
Lobotomy2012 <luongnaman0906.amongus@gmail.com>
2025-09-20 13:59:57 +0700

    Update pyscratch.py

--- PyScratch/pyscratch.py
+++ PyScratch/pyscratch.py
@@ -1,6 +1,6 @@
-from Lib.PyScratch.run import run
-from Lib.PyScratch.Sprite.sprite import Sprite
-from Lib.PyScratch.Sprite.code import Code
+from .run import run
+from .Sprite.sprite import Sprite
+from .Sprite.code import Code
 
 def init():
     import pygame
@@ -9,4 +9,5 @@ def init():
 
     print("\033[96m==== Welcome to PyScratch ====")
     print("\033[94mhttps://github.com/Lobotomy2012/PyScratch/tree/main\033[0m")
+
 
