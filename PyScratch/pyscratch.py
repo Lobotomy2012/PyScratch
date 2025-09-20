1f1e9155a987480915f09db0495ad5e4bb65de03
Lobotomy2012 <luongnaman0906.amongus@gmail.com>
2025-09-20 12:39:57 +0700

    just upload

---
+++ PyScratch/pyscratch.py
@@ -0,0 +1,9 @@
+from Lib.PyScratch.run import run
+from Lib.PyScratch.Sprite.sprite import Sprite
+from Lib.PyScratch.Sprite.code import Code
+
+def init():
+    import pygame
+
+    pygame.init()
+    print("==== Welcome to PyScratch ====")
